a6b251b40c54ee838addbfb1efcc8f5249522b9b
Martin Goth <martin@goth-1.de>
2016-01-27 19:30:36 +0100

    Send Mails using nodemailer

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var eejs = require('ep_etherpad-lite/node/eejs');
+var nodemailer = require('nodemailer');
+var settings = require('ep_etherpad-lite/node/utils/Settings');
 
 exports.eejsBlock_exportColumn = function(hook_name, args, cb){
     args.content = args.content + eejs.require("ep_sendmail/templates/exportcolumn.ejs", {}, module);
@@ -13,5 +15,26 @@ exports.eejsBlock_scripts = function (hook_name, args, cb) {
 exports.expressCreateServer = function (hook_name, args, cb) {
     args.app.get('/p/:pad/:rev?/export/mail', function(req, res, next) {
         var padID = req.params.pad;
+        var emailOptions = (settings.ep_sendmail || {}).config;
+        if(emailOptions){
+            var transporter = nodemailer.createTransport(emailOptions);
+
+            // setup e-mail data with unicode symbols 
+            var mailOptions = {
+                from: 'Fred Foo <martin@goth-1.de>', // sender address 
+                to: ['ep-sendmail@ziisch.de'], // list of receivers 
+                subject: 'Pad ' + padID, // Subject line 
+                text: 'Hello world', // plaintext body 
+                html: '<b>Hello world</b>' // html body 
+            };
+             
+            // send mail with defined transport object 
+            transporter.sendMail(mailOptions, function(error, info){
+                if(error){
+                    return console.log(info+"\n"+error);
+                }
+                console.log('Message sent: ' + info.response);
+            });
+        }
     });
 };
