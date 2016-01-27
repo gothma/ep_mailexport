993da4f3eb688dc79087cb49d66fcc3b66c09115
Martin Goth <martin@goth-1.de>
2016-01-27 19:03:13 +0100

    Rename template files

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ exports.eejsBlock_exportColumn = function(hook_name, args, cb){
 }
 
 exports.eejsBlock_scripts = function (hook_name, args, cb) {
-    args.content = args.content + eejs.require("ep_sendmail/templates/scripts.html", {}, module);
+    args.content = args.content + eejs.require("ep_sendmail/templates/scripts.ejs", {}, module);
     return cb();
 };
 
 exports.expressCreateServer = function (hook_name, args, cb) {
     args.app.get('/p/:pad/:rev?/export/mail', function(req, res, next) {
         var padID = req.params.pad;
-        console.log(padID);
     });
 };
